6e2ff7da81442b683d12cffb63679c32cc0378fe
umaan1982 <60696114+umaan1982@users.noreply.github.com>
2024-02-25 01:27:40 +0500

    Added UI to the Trainer page, and fixed the functionality in Admin page where the admin was not able to add a trainer

--- ElementFitness.App/Pages/Trainers.cshtml.cs
+++ ElementFitness.App/Pages/Trainers.cshtml.cs
@@ -1,19 +1,30 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using ElementFitness.BL.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
 
 namespace ElementFitness.App.Pages
 {
 
     public class TrainerModel : PageModel
     {
+        private readonly ITrainerService _trainerService;
+        public IEnumerable<Models.Trainer>? Trainers { get; private set; }
 
-        public TrainerModel()
+        public TrainerModel(ITrainerService trainerService)
         {
+            _trainerService = trainerService;
         }
 
         public void OnGet()
         {
+            try
+            {
+                Trainers = _trainerService.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
         }
     }
 }
