b0761b013d8827f3a3460d7f17ace9e96466ea28
umaan1982 <60696114+umaan1982@users.noreply.github.com>
2024-02-25 14:32:04 +0500

    Completed the UI of Testimonials page

--- ElementFitness.App/Pages/Testimonials.cshtml.cs
+++ ElementFitness.App/Pages/Testimonials.cshtml.cs
@@ -1,18 +1,28 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using ElementFitness.BL.Interfaces;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
 
 namespace ElementFitness.App.Pages
 {
     public class TestimonialModel : PageModel
     {
-
-        public TestimonialModel()
+        private readonly ITestimonialService _testimonialService;
+        public IEnumerable<Models.Testimonial>? Testimonials { get; private set; }
+        public TestimonialModel(ITestimonialService testimonialService)
         {
+            _testimonialService = testimonialService;
         }
 
         public void OnGet()
         {
+            try
+            {
+                Testimonials = _testimonialService.GetAll();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+            }
         }
     }
 }
