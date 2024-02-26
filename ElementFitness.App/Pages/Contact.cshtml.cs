fe4045c697ccac5e5a01c8c70fa779c83ffbe4ca
umaan1982 <60696114+umaan1982@users.noreply.github.com>
2024-02-27 00:34:12 +0500

    Added General Enquiry button and added Form functionality

--- ElementFitness.App/Pages/Contact.cshtml.cs
+++ ElementFitness.App/Pages/Contact.cshtml.cs
@@ -1,19 +1,74 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using ElementFitness.BL.Interfaces;
+using ElementFitness.Utils.Exceptions;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Serilog;
+using ElementFitness.Models;
 
 namespace ElementFitness.App.Pages
 {
 
     public class ContactModel : PageModel
     {
-
-        public ContactModel()
+        private readonly IEnquiryService _enquiryService;
+        [BindProperty]
+        public Enquiry? enquiryToBeAdded { get; set; }
+        public ContactModel(IEnquiryService enquiryService)
         {
+            _enquiryService = enquiryService;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
+            try
+            {
+                return Page();
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                return RedirectToPage("../../Error");
+            }
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    IEnumerable<string> allErrors = ModelState.Values.SelectMany(v => v.Errors.Select(b => b.ErrorMessage));
+                    string errorMessage = "";
+                    foreach (string error in allErrors)
+                    {
+                        errorMessage += $"•{error} \\n";
+                    }
+                    throw new InvalidModelException(errorMessage);
+                }
+
+                try
+                {
+                    await _enquiryService.AddAsync(enquiryToBeAdded)!;
+                    enquiryToBeAdded = null;
+                    return RedirectToPage("./Index");
+                }
+                catch (Exception ex)
+                {
+                    enquiryToBeAdded = null;
+                    throw new DatabaseException($"An error occurred while adding the Partner. Please try again later.");
+                }
+            }
+            catch (Exception ex)
+            {
+                if (ex is DatabaseException || ex is InvalidModelException || ex is UploadException)
+                {
+                    ViewData["ErrorMessage"] = ex.Message;
+                    return OnGet();
+                }
+
+                Log.Error(ex.Message);
+                return RedirectToPage("../../Error");
+            }
         }
     }
 }
---
+++ ElementFitness.App/wwwroot/js/contact.js
@@ -0,0 +1,50 @@
+﻿const generalenqueries = () => {
+    $("#program-block").attr("hidden", true);
+    $("#map-detail-block").attr("hidden", true);
+    $("#general-enquiry-block").attr("hidden", false);
+}
+
+$("#FirstNameInput").on("keyup", () => {
+    const firstnameInput = $("#FirstNameInput").val();
+    $("#add-first-name-hint").text(`${firstnameInput.length}/50 characters`);
+});
+
+$("#LastNameInput").on("keyup", () => {
+    const lastnameInput = $("#LastNameInput").val();
+    $("#add-last-name-hint").text(`${lastnameInput.length}/50 characters`);
+});
+
+$("#EmailInput").on("keyup", () => {
+    const emailInput = $("#EmailInput").val();
+    $("#add-email-hint").text(`${emailInput.length}/100 characters`);
+});
+
+$("#MobileNumberInput").on("keyup", () => {
+    const mobileInput = $("#MobileNumberInput").val();
+    $("#add-mobile-number-hint").text(`${mobileInput.length}/50 characters`);
+});
+
+$("#MessageInput").on("keyup", () => {
+    const messageInput = $("#MessageInput").val();
+    $("#add-message-hint").text(`${messageInput.length}/400 characters`);
+});
+
+const checkInput = () => {
+    const firstnameInput = $("#FirstNameInput").val().trim();
+    const lastnameInput = $("#LastNameInput").val().trim();
+    const emailInput = $("#EmailInput").val().trim();
+    const mobileNumberInput = $("#MobileNumberInput").val().trim();
+
+    if (
+        firstnameInput !== "" &&
+        lastnameInput !== "" &&
+        emailInput !== "" &&
+        mobileNumberInput !== ""
+    ) {
+        $("#disabled-add-modal-add-btn").attr("hidden", true);
+        $("#add-modal-add-btn").attr("hidden", false);
+    } else {
+        $("#disabled-add-modal-add-btn").attr("hidden", false);
+        $("#add-modal-add-btn").attr("hidden", true);
+    }
+};
