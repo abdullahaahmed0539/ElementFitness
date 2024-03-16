ab651d7d027a1b8c243edb6ce603360cc46dfb0e
umaan1982 <60696114+umaan1982@users.noreply.github.com>
2024-03-16 22:09:32 +0500

    Fixed some UI issues

--- ElementFitness.App/wwwroot/js/layout.js
+++ ElementFitness.App/wwwroot/js/layout.js
@@ -8,20 +8,24 @@ $(document).ready(() => {
   if (page === undefined)
     page = "Home"
 
-  if (page.startsWith("Home")) $("#home-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("AboutUs"))
-    $("#about-us-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("Trainers"))
-    $("#trainers-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("Memberships"))
-    $("#memberships-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("Partners"))
-    $("#partners-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("Testimonials"))
-    $("#testimonials-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("Careers"))
-    $("#careers-link").addClass("half-a-border-on-top");
-  else if (page.startsWith("Contact"))
+    if (page.startsWith("Home")) $("#home-link").addClass("half-a-border-on-top");
+    else if (page.startsWith("AboutUs"))
+        $("#about-us-link").addClass("half-a-border-on-top");
+    else if (page.startsWith("Trainers"))
+        $("#trainers-link").addClass("half-a-border-on-top");
+    else if (page.startsWith("Memberships"))
+        $("#memberships-link").addClass("half-a-border-on-top");
+    else if (page.startsWith("Partners"))
+        $("#partners-link").addClass("half-a-border-on-top");
+    else if (page.startsWith("Testimonials"))
+        $("#testimonials-link").addClass("half-a-border-on-top");
+    else if (page.startsWith("Careers")) {
+        $("#careers-link").addClass("half-a-border-on-top");
+        document.querySelector('.footer').style.position = "absolute";
+        document.querySelector('.footer').style.width = "100%";
+        document.querySelector('.footer').style.bottom = "0";
+    }
+    else if (page.startsWith("Contact"))
     $("#contact-link").addClass("half-a-border-on-top");
-  else $("#home-link").addClass("half-a-border-on-top");
+    else $("#home-link").addClass("half-a-border-on-top");
 });
